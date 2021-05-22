d2b7f852f9b0b30e50dba9f13d8f6702caa1103a
myckael <myckael.and@gmail.com>
2021-05-21 22:48:12 -0300

    iniat commit
    alteração

---
+++ Programa Java/js/main.js
@@ -0,0 +1,31 @@
+ function load() {
+     alert("Página Carregada");
+ }
+
+
+function  clicou() {
+    document.getElementById("agradecimento").innerHTML = "OBRIGADO POR CLICAR";
+}
+
+function trocar(){
+    document.getElementById("agradecimento").innerHTML = "CLIQUE AKI";
+}
+
+
+function redirecionar(){
+    window.open("");
+}
+
+/*
+var idade = prompt("Qual a sua Idade?");
+if (idade >= 18){
+alert("maior de idade");
+}else{
+alert("menor de idade");
+}
+*/
+
+/*
+var nome = "Myckael";
+alert("Primeiro Js");
+*/
